fe0a6df0a300746efd31ed28f04b6d678cdc756f
JBDooley <110794211+JBDooley@users.noreply.github.com>
2023-02-08 13:46:24 -0700

    added color coded feature script

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function displayTime() {
 };
 
 setInterval(displayTime, 1000);
+
+function timeBlockStatus() {
+    $(".timeBlockInput").each(function() {
+        var timeStatus = moment().format("H");
+        console.log(timeStatus);
+        var inputTime = parseInt($(this).attr("value"));
+        // Compare each timeblock to current hour and change color
+        if (inputTime < timeStatus) {
+            $(this).addClass("past");
+        }else if (inputTime == timeStatus) {
+            $(this).addClass("present");
+        }else if (inputTime > timeStatus) {
+            $(this).addClass("future");
+        }
+    })
+}
+
+setInterval(timeBlockStatus, 1000);
